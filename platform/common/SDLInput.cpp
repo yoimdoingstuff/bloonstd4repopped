d1aa41f6457280c6ec30c57b85c7c8e04bc29803
yoimdoingstuff <leafoundas@outlook.com>
2026-09-16 23:10:29 +0930

    Implement platform-specific SDL frontend controls

--- platform/common/SDLInput.cpp
+++ platform/common/SDLInput.cpp
@@ -2,7 +2,16 @@
 
 namespace btd4 {
 
-SDLInput::SDLInput() = default;
+SDLInput::SDLInput(FrontendProfile profile)
+    : m_profile(profile) {
+}
+
+SDLInput::~SDLInput() {
+    if (m_controller) {
+        SDL_GameControllerClose(m_controller);
+        m_controller = nullptr;
+    }
+}
 
 void SDLInput::beginFrame() {
     m_previousActions = m_currentActions;
@@ -26,50 +35,68 @@ PointerState SDLInput::pointerState() const {
     return m_pointer;
 }
 
+void SDLInput::setProfile(FrontendProfile profile) {
+    m_profile = profile;
+    m_keyboardActions = 0;
+    m_pointerActions = 0;
+    m_controllerActions = 0;
+    m_keyDown.fill(false);
+    m_keyActionCounts.fill(0);
+    updateCurrentActions();
+}
+
 void SDLInput::mapKey(SDL_Keycode key, SDL_Scancode scancode, bool isDown) {
     InputAction action = InputAction::None;
 
-    switch (key) {
-        case SDLK_RETURN:
-        case SDLK_SPACE:
-            action = InputAction::Confirm;
+    switch (m_profile) {
+        case FrontendProfile::FlashDesktop:
+            switch (key) {
+                case SDLK_RETURN:
+                case SDLK_SPACE: action = InputAction::Confirm; break;
+                case SDLK_ESCAPE:
+                case SDLK_BACKSPACE: action = InputAction::Cancel; break;
+                case SDLK_p:
+                case SDLK_PAUSE: action = InputAction::Pause; break;
+                case SDLK_r: action = InputAction::StartRound; break;
+                case SDLK_u: action = InputAction::Upgrade; break;
+                case SDLK_s: action = InputAction::Sell; break;
+                case SDLK_UP: action = InputAction::MoveUp; break;
+                case SDLK_DOWN: action = InputAction::MoveDown; break;
+                case SDLK_LEFT: action = InputAction::MoveLeft; break;
+                case SDLK_RIGHT: action = InputAction::MoveRight; break;
+                case SDLK_TAB: action = InputAction::NextTarget; break;
+                case SDLK_1: action = InputAction::SelectTower1; break;
+                case SDLK_2: action = InputAction::SelectTower2; break;
+                case SDLK_3: action = InputAction::SelectTower3; break;
+                case SDLK_4: action = InputAction::SelectTower4; break;
+                case SDLK_5: action = InputAction::SelectTower5; break;
+                case SDLK_6: action = InputAction::SelectTower6; break;
+                default: break;
+            }
+            if (key == SDLK_w) action = InputAction::MoveUp;
+            if (key == SDLK_a) action = InputAction::MoveLeft;
+            if (key == SDLK_s) action = InputAction::MoveDown;
+            if (key == SDLK_d) action = InputAction::MoveRight;
             break;
-        case SDLK_ESCAPE:
-        case SDLK_BACKSPACE:
-            action = InputAction::Cancel;
-            break;
-        case SDLK_p:
-        case SDLK_PAUSE:
-            action = InputAction::Pause;
-            break;
-        case SDLK_r:
-            action = InputAction::StartRound;
-            break;
-        case SDLK_u:
-            action = InputAction::Upgrade;
-            break;
-        case SDLK_s:
-            action = InputAction::Sell;
-            break;
-        case SDLK_UP:
-        case SDLK_w:
-            action = InputAction::MoveUp;
-            break;
-        case SDLK_DOWN:
-            action = InputAction::MoveDown;
-            break;
-        case SDLK_LEFT:
-        case SDLK_a:
-            action = InputAction::MoveLeft;
-            break;
-        case SDLK_RIGHT:
-        case SDLK_d:
-            action = InputAction::MoveRight;
-            break;
-        case SDLK_TAB:
-            action = InputAction::NextTarget;
-            break;
-        default:
+
+        case FrontendProfile::PSP:
+        case FrontendProfile::XboxConsole:
+            // Keyboard remains useful for desktop controller testing, but follows
+            // the console action layout rather than the Flash keyboard layout.
+            switch (key) {
+                case SDLK_RETURN: action = InputAction::Confirm; break;
+                case SDLK_ESCAPE: action = InputAction::Cancel; break;
+                case SDLK_p: action = InputAction::Pause; break;
+                case SDLK_r: action = InputAction::StartRound; break;
+                case SDLK_u: action = InputAction::Upgrade; break;
+                case SDLK_s: action = InputAction::Sell; break;
+                case SDLK_UP: action = InputAction::MoveUp; break;
+                case SDLK_DOWN: action = InputAction::MoveDown; break;
+                case SDLK_LEFT: action = InputAction::MoveLeft; break;
+                case SDLK_RIGHT: action = InputAction::MoveRight; break;
+                case SDLK_TAB: action = InputAction::NextTarget; break;
+                default: break;
+            }
             break;
     }
 
@@ -79,28 +106,102 @@ void SDLInput::mapKey(SDL_Keycode key, SDL_Scancode scancode, bool isDown) {
     }
 
     m_keyDown[scancodeIndex] = isDown;
-    uint32_t actionMask = static_cast<uint32_t>(action);
+    const uint32_t actionMask = static_cast<uint32_t>(action);
     size_t actionIndex = 0;
-    while (actionMask > 1) {
-        actionMask >>= 1;
+    uint32_t mask = actionMask;
+    while (mask > 1) {
+        mask >>= 1;
         ++actionIndex;
     }
 
     uint16_t& keyCount = m_keyActionCounts[actionIndex];
     if (isDown) {
         ++keyCount;
-        m_keyboardActions |= static_cast<uint32_t>(action);
+        m_keyboardActions |= actionMask;
     } else if (keyCount > 0 && --keyCount == 0) {
-        m_keyboardActions &= ~static_cast<uint32_t>(action);
+        m_keyboardActions &= ~actionMask;
     }
     updateCurrentActions();
 }
 
+void SDLInput::mapControllerButton(SDL_GameControllerButton button, bool isDown) {
+    InputAction action = InputAction::None;
+
+    switch (m_profile) {
+        case FrontendProfile::PSP:
+            switch (button) {
+                case SDL_CONTROLLER_BUTTON_A: action = InputAction::Confirm; break;      // Cross
+                case SDL_CONTROLLER_BUTTON_B: action = InputAction::Cancel; break;       // Circle
+                case SDL_CONTROLLER_BUTTON_X: action = InputAction::Sell; break;         // Square
+                case SDL_CONTROLLER_BUTTON_Y: action = InputAction::Upgrade; break;      // Triangle
+                case SDL_CONTROLLER_BUTTON_BACK: action = InputAction::Pause; break;     // Select
+                case SDL_CONTROLLER_BUTTON_START: action = InputAction::StartRound; break;
+                case SDL_CONTROLLER_BUTTON_DPAD_UP: action = InputAction::MoveUp; break;
+                case SDL_CONTROLLER_BUTTON_DPAD_DOWN: action = InputAction::MoveDown; break;
+                case SDL_CONTROLLER_BUTTON_DPAD_LEFT: action = InputAction::PrevTarget; break;
+                case SDL_CONTROLLER_BUTTON_DPAD_RIGHT: action = InputAction::NextTarget; break;
+                case SDL_CONTROLLER_BUTTON_LEFTSHOULDER: action = InputAction::PrevTarget; break;
+                case SDL_CONTROLLER_BUTTON_RIGHTSHOULDER: action = InputAction::NextTarget; break;
+                default: break;
+            }
+            break;
+
+        case FrontendProfile::XboxConsole:
+            switch (button) {
+                case SDL_CONTROLLER_BUTTON_A: action = InputAction::Confirm; break;
+                case SDL_CONTROLLER_BUTTON_B: action = InputAction::Cancel; break;
+                case SDL_CONTROLLER_BUTTON_X: action = InputAction::Sell; break;
+                case SDL_CONTROLLER_BUTTON_Y: action = InputAction::Upgrade; break;
+                case SDL_CONTROLLER_BUTTON_START: action = InputAction::Pause; break;
+                case SDL_CONTROLLER_BUTTON_BACK: action = InputAction::Cancel; break;
+                case SDL_CONTROLLER_BUTTON_DPAD_UP: action = InputAction::MoveUp; break;
+                case SDL_CONTROLLER_BUTTON_DPAD_DOWN: action = InputAction::MoveDown; break;
+                case SDL_CONTROLLER_BUTTON_DPAD_LEFT: action = InputAction::PrevTarget; break;
+                case SDL_CONTROLLER_BUTTON_DPAD_RIGHT: action = InputAction::NextTarget; break;
+                case SDL_CONTROLLER_BUTTON_LEFTSHOULDER: action = InputAction::PrevTarget; break;
+                case SDL_CONTROLLER_BUTTON_RIGHTSHOULDER: action = InputAction::StartRound; break;
+                default: break;
+            }
+            break;
+
+        case FrontendProfile::FlashDesktop:
+            break;
+    }
+
+    if (action == InputAction::None) {
+        return;
+    }
+
+    const uint32_t mask = static_cast<uint32_t>(action);
+    if (isDown) {
+        m_controllerActions |= mask;
+    } else {
+        m_controllerActions &= ~mask;
+    }
+    updateCurrentActions();
+}
+
+void SDLInput::updateControllerDevice(const SDL_Event& event) {
+    if (event.type == SDL_CONTROLLERDEVICEADDED && !m_controller) {
+        m_controller = SDL_GameControllerOpen(event.cdevice.which);
+    } else if (event.type == SDL_CONTROLLERDEVICEREMOVED && m_controller) {
+        SDL_Joystick* joystick = SDL_GameControllerGetJoystick(m_controller);
+        if (joystick && SDL_JoystickInstanceID(joystick) == event.cdevice.which) {
+            SDL_GameControllerClose(m_controller);
+            m_controller = nullptr;
+            m_controllerActions = 0;
+            updateCurrentActions();
+        }
+    }
+}
+
 void SDLInput::updateCurrentActions() {
-    m_currentActions = m_keyboardActions | m_pointerActions;
+    m_currentActions = m_keyboardActions | m_pointerActions | m_controllerActions;
 }
 
 void SDLInput::processEvent(const SDL_Event& event, const Viewport& viewport) {
+    updateControllerDevice(event);
+
     if (event.type == SDL_KEYDOWN) {
         if (!event.key.repeat) {
             mapKey(event.key.keysym.sym, event.key.keysym.scancode, true);
@@ -129,6 +230,10 @@ void SDLInput::processEvent(const SDL_Event& event, const Viewport& viewport) {
             m_pointerActions &= ~static_cast<uint32_t>(InputAction::Cancel);
         }
         updateCurrentActions();
+    } else if (event.type == SDL_CONTROLLERBUTTONDOWN) {
+        mapControllerButton(static_cast<SDL_GameControllerButton>(event.cbutton.button), true);
+    } else if (event.type == SDL_CONTROLLERBUTTONUP) {
+        mapControllerButton(static_cast<SDL_GameControllerButton>(event.cbutton.button), false);
     }
 }
 
