f8fb53f09e3bdda06a00f5b40d7110579e36b84c
yoimdoingstuff <leafoundas@outlook.com>
2026-09-17 20:03:43 +0930

    Wire upgrade data into engine

--- engine/core/Engine.hpp
+++ engine/core/Engine.hpp
@@ -9,6 +9,7 @@
 #include "../input/InputActions.hpp"
 #include "../input/FrontendProfile.hpp"
 #include "../game/GameState.hpp"
+#include "../game/Upgrade.hpp"
 #include "../assets/AssetManager.hpp"
 
 namespace btd4 {
@@ -21,10 +22,7 @@ public:
 
     bool initialize(int windowWidth, int windowHeight);
     void shutdown();
-
-    // Executes a single simulation & render frame
     void frame(int windowWidth, int windowHeight);
-
     void onResize(int windowWidth, int windowHeight);
 
     bool isRunning() const;
@@ -32,9 +30,7 @@ public:
 
     Clock& clock();
     const Clock& clock() const;
-
     const Viewport& currentViewport() const;
-
     FrontendProfile frontendProfile() const { return m_frontendProfile; }
 
     GameSimulation& simulation() { return m_simulation; }
@@ -45,17 +41,19 @@ public:
     void cancelPlacement();
 
 private:
+    bool applySelectedUpgrade(uint8_t path);
+
     IRenderer& m_renderer;
     IInput& m_input;
     FrontendProfile m_frontendProfile{FrontendProfile::FlashDesktop};
     Clock m_clock;
     TestScreen m_testScreen;
     GameSimulation m_simulation;
+    UpgradeSet m_upgrades;
 
     Viewport m_viewport;
     bool m_running{false};
     bool m_gameMode{true};
-
     bool m_hasPlacement{false};
     TowerType m_placementType{TowerType::DartMonkey};
     uint32_t m_selectedTowerId{0};
