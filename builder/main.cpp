dd4e8ff69dab726b1d9017382a2edfe8c09b4ab0
yoimdoingstuff <leafoundas@outlook.com>
2026-09-17 20:10:38 +0930

    Fix portable builder environment variable

--- builder/main.cpp
+++ builder/main.cpp
@@ -11,7 +11,7 @@
 namespace {
 void setBuilderRootEnvironment(const std::filesystem::path& root) {
 #ifdef _WIN32
-    _putenv_s("BTD4_BUILDER_ROOT=" + root.string());
+    _putenv_s("BTD4_BUILDER_ROOT", root.string().c_str());
 #else
     setenv("BTD4_BUILDER_ROOT", root.string().c_str(), 1);
 #endif
