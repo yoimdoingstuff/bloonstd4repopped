7c405a815e30a36a2aab6415bd6e2aa1cf78e6f3
yoimdoingstuff <leafoundas@outlook.com>
2026-09-18 12:24:10 +0930

    Show tower editor in Game Builder

--- builder/ui/BuilderUI.cpp
+++ builder/ui/BuilderUI.cpp
@@ -279,6 +279,7 @@ BuilderUI::BuilderUI() {
 void BuilderUI::initialize() {
     m_mapEditor.initialize();
     m_roundEditor.initialize();
+    m_towerEditor.initialize();
     const fs::path projectRoot = findProjectRoot();
     if (!projectRoot.empty()) {
         const fs::path projectFile = projectRoot / "project.btd4proj";
@@ -349,6 +350,8 @@ void BuilderUI::render() {
         m_mapEditor.render(projectRoot.string());
         ImGui::Spacing();
         m_roundEditor.render(projectRoot.string(), m_mapEditor.map());
+        ImGui::Spacing();
+        m_towerEditor.render(projectRoot.string());
         ImGui::Spacing();
         renderActionButtons();
 
